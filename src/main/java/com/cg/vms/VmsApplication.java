7ca57072f245150c79eb8a3555e94ae178e29c28
Acer <Acer@DESKTOP-JIS7U5C>
2021-05-13 21:15:57 +0530

    Intial version

---
+++ src/main/java/com/cg/vms/VmsApplication.java
@@ -0,0 +1,28 @@
+package com.cg.vms;
+
+import org.apache.logging.log4j.LogManager;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class VmsApplication {
+	org.apache.logging.log4j.Logger logger = LogManager.getLogger();
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmsApplication.class, args);
+	}
+
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+			.select()
+			.paths(PathSelectors.any())
+			.build();
+	}
+}
